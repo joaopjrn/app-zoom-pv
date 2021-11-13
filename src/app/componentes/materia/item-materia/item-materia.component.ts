e5e9991a45c22579285f2db0522ba8e830471bbd
João Pedro J. R. N <joaopjrn@yahoo.com.br>
2021-11-13 16:00:15 -0300

    imagens matérias

--- src/app/componentes/materia/item-materia/item-materia.component.ts
+++ src/app/componentes/materia/item-materia/item-materia.component.ts
@@ -12,10 +12,31 @@ import { ExcluirComponent } from '../../modais/excluir/excluir.component';
 })
 export class ItemMateriaComponent implements OnInit {
   @Input() materia: Materia;
+  hue: string;
+
+  imgs: string[] = [
+    "../../../../assets/img/bio-1.png",
+    "../../../../assets/img/bio-2.png",
+    "../../../../assets/img/exatas-1.png",
+    "../../../../assets/img/exatas-2.png",
+    "../../../../assets/img/humanas-1.png",
+    "../../../../assets/img/saude-1.png",
+    "../../../../assets/img/saude-2.png",
+    "../../../../assets/img/tech-1.png",
+    "../../../../assets/img/tech-2.png"
+  ];
 
   constructor(private modal: MatDialog) { }
 
   ngOnInit(): void {
+    this.materia.linkImg = this.imgs[this.getRandomInt(0, (this.imgs.length - 1))];
+    this.hue = `filter:hue-rotate(${this.getRandomInt(0,360)}deg);`
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
   }
 
   excluirMateria(){
